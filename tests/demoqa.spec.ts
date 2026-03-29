f18e3527975b98c4fa4573c010542354459108ce
kabboCSE <shahriar15-10494@diu.edu.bd>
2026-03-29 17:59:35 +0600

    Add faker dependency and update test cases for user creation and button interactions

--- tests/demoqa.spec.ts
+++ tests/demoqa.spec.ts
@@ -7,27 +7,27 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/demositeee/);
 });
-test('has title1', async ({ page }) => {
-  await page.goto('https://demoqa.com/', {timeout: 2000});
-  // timeout = navigation timeout
-  
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/demositeee/);
-});
-test('has title2', async ({ page }) => {
-  await page.goto('https://demoqa.com/', {timeout: 2000});
-  // timeout = navigation timeout
-  
+import { generateRandomNumber } from "../utils/utils.ts";
+import { faker } from '@faker-js/faker';
+import path from "path";
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/demositeee/);
+test("Create new user", async ({ page }) => {
+  await page.goto("https://demoqa.com/text-box");
+  await page.getByRole("textbox", { name: "Full Name" }).fill(faker.person.fullName());
+  await page.getByPlaceholder("name@example.com").fill(`salmansrabon+${generateRandomNumber(1000,9999)}@gmail.com`)
+  await page.getByRole("button", { name: "Submit" }).click();
+  //await page.pause();
 });
-test('has title3', async ({ page }) => {
-  await page.goto('https://demoqa.com/', {timeout: 2000});
-  // timeout = navigation timeout
-  
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/demositeee/);
+test("button click", async ({ page }) => {
+  await page.goto("https://demoqa.com/buttons");
+  await page.getByRole("button", { name: "Double Click Me" }).dblclick();
+  await page
+    .getByRole("button", { name: "Right Click Me" })
+    .click({ button: "right" });
+  await expect(page.getByText("double click").nth(1)).toContainText(
+    "You have done a double click"
+  );
 });
+
