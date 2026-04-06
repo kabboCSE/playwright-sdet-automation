b6399a7b6de21de58ee484880f1bc6b498697da2
kabboCSE <shahriar15-10494@diu.edu.bd>
2026-04-06 16:29:32 +0600

    Enhance double click test: add right click functionality and assertions

--- tests/demoqa.spec.ts
+++ tests/demoqa.spec.ts
@@ -33,9 +33,18 @@ test("button click", async ({ page }) => {
 
 test.only("double click", async ({page})=>{
     await page.goto("https://demoqa.com/buttons");
-    await page.getByRole("button",{name: "Double Click Me" }).dblclick();
+    await page.getByRole("button",{name: "Double Click Me" }).dblclick();   
     const txtActual = await page.getByText("double click").nth(1).textContent(); // Double click message appears twice in the DOM; nth(1) returns the 2nd matching element (0-indexed), allowing us to retrieve the actual result message
+    // const txtActual = await page.getByText("a double click").nth(1).textContent(); // make it unique to prevent the first element match // this is another way
     expect (txtActual).toContain("You have done a double click")
+
+
+    // Right Click
+    await page.getByRole("button",{name:"Right Click Me"}).click({button:"right"});
+    const txtActualRightBtn = await page.getByText("a right click").textContent();
+    expect (txtActualRightBtn).toContain("You have done a right click")
+
     await page.pause();
-})
+});
+
 
